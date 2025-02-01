80b2bb32d6e03a10f47ac2f8329b4980fcee78b6
Junichi Akita <akita@ifdl.jp>
2025-02-01 16:29:00 +0900

    tentative graph draw (not tested)

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@
 char buf[1024];
 uint8_t p = 0;
 
+// Screen Design
+//T
+//|   graph        : (Temp, Temp_target, Temp_th)
+//+--------------t
+// Time (ElaspTime/Dur) <--- 200
+// 10   50   
+
 float tm01 = 0.0, tm12 = 0.0, tm23 = 0.0, tm = 0.0, 
       temp = 0.0, temp_target = 0.0, temp_th = 0.0,
       dur = 0.0;
@@ -22,15 +29,34 @@ void DrawFloat(float v, int x, int y, float size, uint16_t color)
 
 }
 
+uint16_t temp_conv(float temp)
+{
+  // 20deg - 300deg -> 200 - 0
+  int y = (300 - temp) * 200 / 280;
+  if (y < 0) y = 0;
+  if (y > 200) y = 200;
+  return(y);
+}
+
+uint16_t time_conv(uint16_t t)
+{
+  // 0 - 360 -> 0 - 320
+  int x = t * 320 / 360;
+  if (x < 0) x = 0;
+  if (x > 320) x = 320;
+  return(x);
+}
+
 void ShowInfo(uint16_t bg_color)
 {
-  M5.Display.clearDisplay(bg_color);
-  M5.Display.setTextSize(2.5); M5.Display.drawString("T=", 0, 8);
-  DrawFloat(tm,           90,   8, 2.5, TFT_WHITE);
-  M5.Display.setTextSize(2); M5.Display.drawString("Tmp", 0, 65);
-  DrawFloat(temp,        110,  60,   3, TFT_WHITE);
-  DrawFloat(temp_target,   0, 120, 1.5, TFT_WHITE);
-  DrawFloat(temp_th,      110, 120, 1.5, TFT_WHITE);
+  M5.Display.drawRect(0, 200, 320, 40, bg_color);
+  M5.Display.setTextSize(2.5);
+  M5.Display.setCursor(10, 200); M5.Display.printf("%.1f", tm);
+  if (dur != 0){
+    M5.Display.setCursor(50, 200); M5.Display.printf("(%.1f / %.1f)", tm01, dur);
+  }
+
+/*
   M5.Display.setTextSize(1.5); M5.Display.drawString("Dur", 20, 160);
   DrawFloat(dur,         120, 160, 1.5, TFT_WHITE);
   DrawFloat(tm01,         20, 200, 1.5, TFT_WHITE);
@@ -41,13 +67,20 @@ void ShowInfo(uint16_t bg_color)
 //  DrawFloat(t2, 240, 200, 1.5, TFT_WHITE);
   DrawFloat(tm12, 130, 200, 1.5, TFT_WHITE);
   DrawFloat(tm23, 240, 200, 1.5, TFT_WHITE);
+*/
+  uint16_t x = (int)tm;
+  M5.Display.drawPixel(time_conv(x), temp_conv(temp), TFT_WHITE);
+  M5.Display.drawPixel(time_conv(x), temp_conv(temp_target), TFT_RED);
+  M5.Display.drawPixel(time_conv(x), temp_conv(temp_th), TFT_BLUE);
 
+/*
   char str[80];
   M5.Display.setTextSize(1.0);
   sprintf(str, "%d %d %d", s1, s2, st);
   M5.Display.drawString(str, 250, 5);
   sprintf(str, "%d", ctrl);
   M5.Display.drawString(str, 250, 20);
+*/
 }
 
 void setup() {
@@ -56,8 +89,16 @@ void setup() {
   auto cfg = M5.config(); 
   M5.begin(cfg);
   M5.Display.setFont(&fonts::Font4);
-  ShowInfo(TFT_BLACK);
-}
+  M5.Display.fillScreen(TFT_BLACK);
+  M5.Display.drawFastHLine(0, 200, 320, TFT_WHITE);
+  for (uint16_t tmp = 50; tmp < 300; tmp+=50)
+    M5.Display.drawFastHLine(0, temp_conv(tmp), 320, TFT_DARKGRAY);
+  for (uint16_t t = 0; t < 360; t+=60)
+    M5.Display.drawFastVLine(time_conv(t), 0, 200, TFT_DARKGRAY);
+
+
+  M5.Display.drawFastVLine(200, 0, 200, TFT_WHITE);
+}  
 
 // st fr  st'
 // 0  0  rise 1
